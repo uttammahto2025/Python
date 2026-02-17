94b659776254f7065e91c0d1b8e39f62f333c598
uttammahto2025 <uttamma72@gmail.com>
2026-02-17 15:17:17 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,11 @@
+# List of weights in kilograms
+weights_kg = [50, 75, 120, 30]
+
+# Loop through each weight
+for kg in weights_kg:
+    pounds = kg+15
+    
+    print("-----------------------")
+    print("Weight in kg:", kg)
+    print("Weight in pounds:", pounds)
+    print("-----------------------")
